80eafc2285c62af8b2637dd064bfc04c25f1a180
jasper38 <jasper@torchtechpro.com>
2023-02-20 17:53:29 +0800

    add test rest api

---
+++ my-app/src/routes/tracer/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Tracer owner</title>
+	<meta name="Tracer" content="About this address tracer" />
+</svelte:head>
+
+<script lang="ts">
+    import Owner from './Owner.svelte';
+</script>
+  
+<main>
+    <Owner />
+</main>
---
+++ my-app/src/routes/tracer/Owner.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    async function getOwner() {
+        const res = await fetch("http://127.0.0.1:9999/tracer/owner")
+        const owner = await res.json()
+
+        console.log('Owner is', owner)
+
+        if (res.ok){
+            return owner;
+        }
+        else {
+            throw new Error(owner)
+        }
+    }
+</script>
